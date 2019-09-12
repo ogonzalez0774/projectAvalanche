b3aae8b2f7d69f02e9f4249747860ca06bdcdd8c
Todd Tracy <trvcytg@gmail.com>
2019-09-11 20:10:27 -0500

    Added base API referencing language

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,16 @@
 console.log("projectAVALANCHE");
+
+var queryURL = `https://api.spotify.com/v1/playlists/${playlist_id}&api_key=f5b0995a65cc4dde87fa7cbb1fe2d1b6`;
+
+// Performing an AJAX request with the queryURL
+$.ajax({
+url: queryURL,
+method: "GET"
+})
+// After data comes back from the request
+.then(function(response) {
+  console.log(queryURL);
+
+  console.log(response);
+  // storing the data from the AJAX request in the results variable
+  var results = response.data;
