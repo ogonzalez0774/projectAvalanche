0aba2d062b73c268a5192f7a462c4099be8217ce
Todd Tracy <trvcytg@gmail.com>
2019-09-11 21:18:10 -0500

    Commented out Spotify API, added weather API

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,16 +1,52 @@
 console.log("projectAVALANCHE");
 
-var queryURL = `https://api.spotify.com/v1/playlists/${playlist_id}&api_key=f5b0995a65cc4dde87fa7cbb1fe2d1b6`;
+// let playlist_id = "37i9dQZF1DX7KNKjOK0o75";
+// var queryURL = `https://api.spotify.com/v1/playlists/${playlist_id}`;
 
-// Performing an AJAX request with the queryURL
-$.ajax({
-url: queryURL,
-method: "GET"
-})
-// After data comes back from the request
-.then(function(response) {
-  console.log(queryURL);
+// // Performing an AJAX request with the queryURL
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// })
+//   // After data comes back from the request
+//   .then(function(response) {
+//     console.log(queryURL);
 
-  console.log(response);
-  // storing the data from the AJAX request in the results variable
-  var results = response.data;
+//     console.log(response);
+//     // storing the data from the AJAX request in the results variable
+//     var results = response.data;
+//     console.log(results);
+//   });
+
+    // This is our API key
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    let locationInfo = $(`#typearea`);
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      `q=${locationInfo}&units=imperial&appid=` + APIKey;
+
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function(response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
+
