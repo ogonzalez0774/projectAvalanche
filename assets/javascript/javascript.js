f59cf9e0cde6aee1c5305c67544293c2d88ce08f
ogonzalez0774 <52640189+ogonzalez0774@users.noreply.github.com>
2020-06-12 04:25:40 -0500

    Update javascript.js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -64,7 +64,7 @@ $("#enter").on("click", function(e) {
   // }
 
   // This is our API key
-  var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  var APIKey = "";
   let locationInfo = $(`#typearea`).val();
   // Here we are building the URL we need to query the database
   var queryURL =
